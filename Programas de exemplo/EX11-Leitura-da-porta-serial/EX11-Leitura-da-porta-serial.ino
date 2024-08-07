d208c39b96ccabd7a6eba0e3252e43749189be45
Epaminondas Lage <epaminondaslage@gmail.com>
2024-08-07 15:46:50 -0300

    ajustes de exemplos

---
+++ Programas de exemplo/EX11-Leitura-da-porta-serial/EX11-Leitura-da-porta-serial.ino
@@ -0,0 +1,22 @@
+int status = 0; //VARIÁVEL QUE CONTROLA O STATUS (LIGADO / DESLIGADO)
+
+void setup(){
+  pinMode(13, OUTPUT); //DECLARA O PINO 13 COMO SENDO SAÍDA
+  digitalWrite(13, LOW); //LED INICIA DESLIGADO
+  Serial.begin(9600); //INICIALIZA A PORTA SERIAL
+}    
+void loop(){
+  char c = Serial.read(); //VARIÁVEL RESPONSÁVEL POR RECEBER O CARACTER DIGITADO NA JANELA SERIAL
+
+  if (c == 'a'){ //SE CARACTER DIGITADO FOR IGUAL A "a", FAZ
+    if (status == 0){ //SE VARIÁVEL FOR IGUAL A 0, FAZ
+      digitalWrite(13, HIGH); //LIGA O LED DO PINO 13
+      status = 1; //VARIÁVEL RECEBE O VALOR 1 (LIGADO)
+      Serial.println("LED 13 LIGADO"); //ESCREVE O TEXTO NA JANELA SERIAL
+    }else{ //SENÃO, FAZ
+      digitalWrite(13, LOW); //DESLIGA O LED DO PINO 13
+      status = 0; //VARIÁVEL RECEBE O VALOR 0 (DESLIGADO)
+      Serial.println("LED 13 DESLIGADO"); //ESCREVE O TEXTO NA JANELA SERIAL
+    }
+  }
+}
