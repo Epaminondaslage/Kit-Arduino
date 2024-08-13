a4c5d8bae78aaeb13a693e0fbf030086b930d839
Epaminondas Lage <44790000+Epaminondaslage@users.noreply.github.com>
2024-08-13 17:04:58 -0300

    Update EX1-Blink.ino

--- Programas de exemplo/EX1-Blink/EX1-Blink.ino
+++ Programas de exemplo/EX1-Blink/EX1-Blink.ino
@@ -1,15 +1,21 @@
 /*
-Blink (Piscar)
+  Blink (Piscar)
+  Este exemplo faz o LED embutido na placa Arduino piscar continuamente.
+  O LED embutido geralmente está conectado ao pino digital 13.
+
+  Elaborado/Adaptado por Epaminondas Lage
 */
+
 // A função setup() é executada uma vez quando você pressiona reset ou liga a placa
 void setup() {
-// inicializa o pino digital LED_BUILTIN como uma saída.
-pinMode(LED_BUILTIN, OUTPUT);
+  // Inicializa o pino digital LED_BUILTIN como uma saída
+  pinMode(LED_BUILTIN, OUTPUT);
 }
+
 // A função loop() é executada continuamente
 void loop() {
-digitalWrite(LED_BUILTIN, HIGH); // acende o LED (HIGH é o nível de tensão)
-delay(1000); // espera por um segundo
-digitalWrite(LED_BUILTIN, LOW); // apaga o LED (LOW é o nível de tensão)
-delay(1000); // espera por um segundo
+  digitalWrite(LED_BUILTIN, HIGH); // Acende o LED (HIGH é o nível de tensão)
+  delay(1000);                     // Espera por um segundo
+  digitalWrite(LED_BUILTIN, LOW);  // Apaga o LED (LOW é o nível de tensão)
+  delay(1000);                     // Espera por um segundo
 }
