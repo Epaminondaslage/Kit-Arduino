661e4bb7a9faafcc701126ce44c29a4056bb6510
Epaminondas Lage <44790000+Epaminondaslage@users.noreply.github.com>
2024-08-13 17:15:29 -0300

    Update EX5-Pisca-3x.ino

--- Programas de exemplo/EX5-Pisca-3x/EX5-Pisca-3x.ino
+++ Programas de exemplo/EX5-Pisca-3x/EX5-Pisca-3x.ino
@@ -1,8 +1,16 @@
+/*
+  Controle de Piscar LED com Intervalos Configuráveis
+  Este exemplo faz um LED conectado ao pino 10 piscar três vezes com duração de 100 ms por piscada,
+  seguido de um período de 1,5 segundos em que o LED permanece apagado.
+
+  Elaborado/Adaptado por Epaminondas Lage
+*/
+
 // Definição das variáveis configuráveis
 const int ledPin = 10;             // Pino onde o LED está conectado
 int blinkCount = 3;                // Número de piscadas
-int blinkDuration = 100;           // Duração de cada piscada em milissegundos (0.1 segundo)
-int offDuration = 1500;            // Duração de apagar o LED em milissegundos (1.5 segundos)
+int blinkDuration = 100;           // Duração de cada piscada em milissegundos (0,1 segundo)
+int offDuration = 1500;            // Duração de apagar o LED em milissegundos (1,5 segundos)
 
 void setup() {
   pinMode(ledPin, OUTPUT);         // Define o pino do LED como saída
