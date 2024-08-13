77dd18b37f84426cad9490a57c0d71b6c04b61bc
Epaminondas Lage <44790000+Epaminondaslage@users.noreply.github.com>
2024-08-13 14:24:32 -0300

    Update exemplo_Matriz_de_led_8x8_Placar_eletronico_I.ino

--- Programas de exemplo/exemplo_Matriz_de_led_8x8_Placar_eletronico_I/exemplo_Matriz_de_led_8x8_Placar_eletronico_I.ino
+++ Programas de exemplo/exemplo_Matriz_de_led_8x8_Placar_eletronico_I/exemplo_Matriz_de_led_8x8_Placar_eletronico_I.ino
@@ -1,88 +1,81 @@
 /**************************************************************************
- * Programa: Placar Eletronico com Arduino e Max7219
+ * Programa: Placar Eletrônico com Arduino e Max7219
  * Autor: Arduino e Cia 
  * Adaptado por Epaminondas Lage
  * https://www.arduinoecia.com.br/2017/11/placar-eletronico-arduino-modulo-max-7219.html?src=Filipeflop
- * Utiliza duas matriz de LED 8x8 com Max 7219
+ * Utiliza duas matrizes de LED 8x8 com Max7219
  * Programa para Arduino UNO/Nano
- * http://xantorohara.github.io/led-matrix-editor/# Editor de caracter
+ * http://xantorohara.github.io/led-matrix-editor/# Editor de caracteres
+ **************************************************************************/
+
+#include "LedControl.h" // Inclui a biblioteca LedControl.h para controlar o Max7219
+
+/* Conexões do módulo Max7219:
+ * 
+ * DATA IN do Max7219 -> Pino 12 do Arduino
+ * CLK do Max7219 -> Pino 11 do Arduino
+ * LOAD(/CS) do Max7219 -> Pino 10 do Arduino
+ * LedControl lc = LedControl(12, 11, 10, 2); // Inicializa o controle para 2 displays
 */
 
+// Definições dos pinos do Arduino para a conexão com o Max7219
+LedControl lc = LedControl(12, 11, 10, 2); // Configura para 2 matrizes de LED 8x8
 
-#include "LedControl.h" // Incluir a bilbioteca LedControl.h
-
-/*Conexoes do modulo Max7219
- *  
- * Create a new LedControl variable.
- * We use pins 12,11 and 10 on the Arduino Nano for the SPI interface
- * Pin 12 is connected to the DATA IN-pin of the first MAX7221
- * Pin 11 is connected to the CLK-pin of the first MAX7221
- * Pin 10 is connected to the LOAD(/CS)-pin of the first MAX7221
- * There will only be a single MAX7221 attached to the arduino 
- * LedControl lc1=LedControl(12,11,10,1);
-*/
-
-//DataIn = Pino 12
-//CLK = Pino 11
-//CS = Pino 10
-
-LedControl lc = LedControl(12, 11, 10, 2);
-
-//Armazenam o valor dos botoes
+// Variáveis para armazenar o estado dos botões
 int v_but1, v_but2, v_but3;
 
-//Brilho dos displays
+// Brilho dos displays
 int brilho = 15;
 
-//Numero do display selecionado
+// Número do display selecionado
 int j = 0;
 
-//Array do placar atual
+// Arrays para armazenar o placar atual e o placar anterior
 int placar[2] = {0, 0};
-
-//Array do placar anterior
 int placar_ant[2] = {0, 0};
 
-unsigned long delaytime = 200;
+unsigned long delaytime = 200; // Tempo de atraso entre as operações
 
 void setup()
 {
-  Serial.begin(9600);
-  //Inicializa pinos botoes
+  Serial.begin(9600); // Inicializa a comunicação serial para depuração
+
+  // Inicializa os pinos dos botões como entradas com pull-up interno
   pinMode(6, INPUT_PULLUP);
   pinMode(7, INPUT_PULLUP);
   pinMode(8, INPUT_PULLUP);
-  //Inicializa pinos leds
+
+  // Inicializa os pinos dos LEDs como saídas
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
-  digitalWrite(3, HIGH);
+  digitalWrite(3, HIGH); // Acende o LED conectado ao pino 3 inicialmente
 
-  //Inicializa os displays
-  lc.shutdown(0, false);
-  lc.shutdown(1, false);
+  // Inicializa os displays de LED
+  lc.shutdown(0, false); // Ativa o primeiro display
+  lc.shutdown(1, false); // Ativa o segundo display
 
-  //Ajusta o brilho de cada display
+  // Configura o brilho dos displays
   lc.setIntensity(0, brilho);
   lc.setIntensity(1, brilho);
 
-  //Apaga os displays
+  // Limpa os displays
   lc.clearDisplay(0);
   lc.clearDisplay(1);
 
-  //Mostra 0 (zero) nos dois displays
+  // Mostra o número 0 em ambos os displays
   writeOnMatrix(0, 0);
   writeOnMatrix(0, 1);
 }
 
 void loop()
 {
-  //Verifica se o botao de alternar
-  //displays foi acionado
+  // Verifica se o botão de alternar displays foi pressionado
   v_but1 = digitalRead(6);
   if (v_but1 == 0)
   {
-    j = !j;
-    //Acende o led correspondente
+    j = !j; // Alterna entre o display 0 e 1
+
+    // Acende o LED correspondente ao display selecionado
     if (j == 0)
     {
       digitalWrite(3, HIGH);
@@ -93,198 +86,118 @@ void loop()
       digitalWrite(3, LOW);
       digitalWrite(4, HIGH);
     }
+
+    // Aguarda até que o botão seja liberado
     while (digitalRead(6) == 0)
-    {delay(50);}
+    {
+      delay(50);
+    }
   }
 
-  //Verifica se o botao de aumentar
-  //contador foi acionado
+  // Verifica se o botão de aumentar o contador foi pressionado
   v_but2 = digitalRead(7);
   if (v_but2 == 0)
   {
-    placar[j] = placar[j] + 1;
+    placar[j] = placar[j] + 1; // Incrementa o placar do display selecionado
     if (placar[j] <= 0 || placar[j] >= 10)
     {
-      placar[j] = 0;
+      placar[j] = 0; // Reseta o placar se for maior que 9 ou menor que 0
     }
     if (placar_ant[j] != placar[j])
     {
-      //Chama a rotina que atualiza o display
+      // Atualiza o display com o novo valor do placar
       writeOnMatrix(placar[j], j);
-      v_but3 = digitalRead(8);
-      placar_ant[j] = placar [j];
+      placar_ant[j] = placar[j]; // Atualiza o placar anterior
     }
+
+    // Aguarda até que o botão seja liberado
     while (digitalRead(7) == 0)
-    {delay(50);}
+    {
+      delay(50);
+    }
   }
 
-  //Verifica se o botao de diminuir
-  //contador foi acionado
+  // Verifica se o botão de diminuir o contador foi pressionado
   v_but3 = digitalRead(8);
   if (v_but3 == 0)
   {
-    placar[j] = placar[j] - 1;
+    placar[j] = placar[j] - 1; // Decrementa o placar do display selecionado
     if (placar[j] <= 0 || placar[j] >= 10)
     {
-      placar[j] = 0;
+      placar[j] = 0; // Reseta o placar se for menor que 0 ou maior que 9
     }
     if (placar_ant[j] != placar[j])
     {
-      //Chama a rotina que atualiza o display
+      // Atualiza o display com o novo valor do placar
       writeOnMatrix(placar[j], j);
-      v_but3 = digitalRead(8);
-      placar_ant[j] = placar [j];
+      placar_ant[j] = placar[j]; // Atualiza o placar anterior
     }
+
+    // Aguarda até que o botão seja liberado
     while (digitalRead(8) == 0)
-    {delay(50);}
+    {
+      delay(50);
+    }
   }
 }
 
+// Função para exibir um número na matriz de LEDs
 void writeOnMatrix(int numero, int tela)
 {
-  //O array abaixo forma o numero 0
+  // Arrays que definem a forma de cada número de 0 a 9
   byte N0[8] = {B00000000, B00000000, B00111110, B01000001,
-                B01000001, B00111110, B00000000, B00000000
-               };
-  //numero 1
+                B01000001, B00111110, B00000000, B00000000};
   byte N1[8] = {B00000000, B00000000, B00000000, B01000010,
-                B01111111, B01000000, B00000000, B00000000
-               };
-  //numero 2
+                B01111111, B01000000, B00000000, B00000000};
   byte N2[8] = {B00000000, B00000000, B01100010, B01010001,
-                B01001001, B01000110, B00000000, B00000000
-               };
-  //numero 3
+                B01001001, B01000110, B00000000, B00000000};
   byte N3[8] = {B00000000, B00000000, B00100010, B01000001,
-                B01001001, B00110110, B00000000, B00000000
-               };
-  //numero 4
+                B01001001, B00110110, B00000000, B00000000};
   byte N4[8] = {B00000000, B00000000, B00001100, B00001010,
-                B00001001, B01111111, B00000000, B00000000
-               };
-  //numero 5
+                B00001001, B01111111, B00000000, B00000000};
   byte N5[8] = {B00000000, B00000000, B00100111, B01000101,
-                B01000101, B00111001, B00000000, B00000000
-               };
-  //numero 6
+                B01000101, B00111001, B00000000, B00000000};
   byte N6[8] = {B00000000, B00000000, B00111110, B01001001,
-                B01001001, B00110000, B00000000, B00000000
-               };
-  //numero 7
+                B01001001, B00110000, B00000000, B00000000};
   byte N7[8] = {B00000000, B00000000, B01100001, B00010001,
-                B00001001, B00000111, B00000000, B00000000
-               };
-  //numero 8
+                B00001001, B00000111, B00000000, B00000000};
   byte N8[8] = {B00000000, B00000000, B00110110, B01001001,
-                B01001001, B00110110, B00000000, B00000000
-               };
-  //numero 9
+                B01001001, B00110110, B00000000, B00000000};
   byte N9[8] = {B00000000, B00000000, B00001110, B01001001,
-                B01001001, B00111110, B00000000, B00000000
-               };
+                B01001001, B00111110, B00000000, B00000000};
 
-  //Mostra no display selecionado o numero correspondente
+  // Mostra no display selecionado o número correspondente
   switch (numero)
   {
     case 0:
-      lc.setRow(tela, 1, N0[0]);
-      lc.setRow(tela, 1, N0[1]);
-      lc.setRow(tela, 2, N0[2]);
-      lc.setRow(tela, 3, N0[3]);
-      lc.setRow(tela, 4, N0[4]);
-      lc.setRow(tela, 5, N0[5]);
-      lc.setRow(tela, 6, N0[6]);
-      lc.setRow(tela, 7, N0[7]);
+      for (int i = 0; i < 8; i++) lc.setRow(tela, i, N0[i]);
       break;
     case 1:
-      lc.setRow(tela, 1, N1[0]);
-      lc.setRow(tela, 1, N1[1]);
-      lc.setRow(tela, 2, N1[2]);
-      lc.setRow(tela, 3, N1[3]);
-      lc.setRow(tela, 4, N1[4]);
-      lc.setRow(tela, 5, N1[5]);
-      lc.setRow(tela, 6, N1[6]);
-      lc.setRow(tela, 7, N1[7]);
+      for (int i = 0; i < 8; i++) lc.setRow(tela, i, N1[i]);
       break;
     case 2:
-      lc.setRow(tela, 1, N2[0]);
-      lc.setRow(tela, 1, N2[1]);
-      lc.setRow(tela, 2, N2[2]);
-      lc.setRow(tela, 3, N2[3]);
-      lc.setRow(tela, 4, N2[4]);
-      lc.setRow(tela, 5, N2[5]);
-      lc.setRow(tela, 6, N2[6]);
-      lc.setRow(tela, 7, N2[7]);
+      for (int i = 0; i < 8; i++) lc.setRow(tela, i, N2[i]);
       break;
     case 3:
-      lc.setRow(tela, 1, N3[0]);
-      lc.setRow(tela, 1, N3[1]);
-      lc.setRow(tela, 2, N3[2]);
-      lc.setRow(tela, 3, N3[3]);
-      lc.setRow(tela, 4, N3[4]);
-      lc.setRow(tela, 5, N3[5]);
-      lc.setRow(tela, 6, N3[6]);
-      lc.setRow(tela, 7, N3[7]);
+      for (int i = 0; i < 8; i++) lc.setRow(tela, i, N3[i]);
       break;
     case 4:
-      lc.setRow(tela, 1, N4[0]);
-      lc.setRow(tela, 1, N4[1]);
-      lc.setRow(tela, 2, N4[2]);
-      lc.setRow(tela, 3, N4[3]);
-      lc.setRow(tela, 4, N4[4]);
-      lc.setRow(tela, 5, N4[5]);
-      lc.setRow(tela, 6, N4[6]);
-      lc.setRow(tela, 7, N4[7]);
+      for (int i = 0; i < 8; i++) lc.setRow(tela, i, N4[i]);
       break;
     case 5:
-      lc.setRow(tela, 1, N5[0]);
-      lc.setRow(tela, 1, N5[1]);
-      lc.setRow(tela, 2, N5[2]);
-      lc.setRow(tela, 3, N5[3]);
-      lc.setRow(tela, 4, N5[4]);
-      lc.setRow(tela, 5, N5[5]);
-      lc.setRow(tela, 6, N5[6]);
-      lc.setRow(tela, 7, N5[7]);
+      for (int i = 0; i < 8; i++) lc.setRow(tela, i, N5[i]);
       break;
     case 6:
-      lc.setRow(tela, 1, N6[0]);
-      lc.setRow(tela, 1, N6[1]);
-      lc.setRow(tela, 2, N6[2]);
-      lc.setRow(tela, 3, N6[3]);
-      lc.setRow(tela, 4, N6[4]);
-      lc.setRow(tela, 5, N6[5]);
-      lc.setRow(tela, 6, N6[6]);
-      lc.setRow(tela, 7, N6[7]);
+      for (int i = 0; i < 8; i++) lc.setRow(tela, i, N6[i]);
       break;
     case 7:
-      lc.setRow(tela, 1, N7[0]);
-      lc.setRow(tela, 1, N7[1]);
-      lc.setRow(tela, 2, N7[2]);
-      lc.setRow(tela, 3, N7[3]);
-      lc.setRow(tela, 4, N7[4]);
-      lc.setRow(tela, 5, N7[5]);
-      lc.setRow(tela, 6, N7[6]);
-      lc.setRow(tela, 7, N7[7]);
+      for (int i = 0; i < 8; i++) lc.setRow(tela, i, N7[i]);
       break;
     case 8:
-      lc.setRow(tela, 1, N8[0]);
-      lc.setRow(tela, 1, N8[1]);
-      lc.setRow(tela, 2, N8[2]);
-      lc.setRow(tela, 3, N8[3]);
-      lc.setRow(tela, 4, N8[4]);
-      lc.setRow(tela, 5, N8[5]);
-      lc.setRow(tela, 6, N8[6]);
-      lc.setRow(tela, 7, N8[7]);
+      for (int i = 0; i < 8; i++) lc.setRow(tela, i, N8[i]);
       break;
     case 9:
-      lc.setRow(tela, 1, N9[0]);
-      lc.setRow(tela, 1, N9[1]);
-      lc.setRow(tela, 2, N9[2]);
-      lc.setRow(tela, 3, N9[3]);
-      lc.setRow(tela, 4, N9[4]);
-      lc.setRow(tela, 5, N9[5]);
-      lc.setRow(tela, 6, N9[6]);
-      lc.setRow(tela, 7, N9[7]);
+      for (int i = 0; i < 8; i++) lc.setRow(tela, i, N9[i]);
       break;
   }
 }
