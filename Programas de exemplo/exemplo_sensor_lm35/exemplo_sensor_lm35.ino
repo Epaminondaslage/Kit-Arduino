e41709e093aa597d3e253970aa86fd9d7ad746d1
Epaminondas Lage <44790000+Epaminondaslage@users.noreply.github.com>
2024-08-13 15:54:02 -0300

    Update exemplo_sensor_lm35.ino

--- Programas de exemplo/exemplo_sensor_lm35/exemplo_sensor_lm35.ino
+++ Programas de exemplo/exemplo_sensor_lm35/exemplo_sensor_lm35.ino
@@ -1,16 +1,34 @@
-float temp; //VARIÁVEL QUE ARMAZENA A TEMPERATURA MEDIDA
-const int sensorPin = A0; //PINO EM QUE O SENSOR ESTÁ CONECTADO
+/**************************************************************************
+                Medição de Temperatura com Sensor LM35 e Arduino
+             Elaborado/Adptado por [Epaminondas Lage]
+              
+  Este código utiliza um sensor LM35 conectado ao pino A0 do Arduino para
+  medir a temperatura ambiente e exibi-la no monitor serial a cada segundo.
+  
+  - A leitura analógica do pino A0 é convertida em uma tensão correspondente.
+  - A tensão é então convertida para uma leitura de temperatura em graus Celsius.
+  
+ **************************************************************************/
+
+float temp; // Variável que armazena a temperatura medida
+const int sensorPin = A0; // Pino em que o sensor está conectado (pino analógico A0)
 
 void setup(){
-  Serial.begin(9600); //INICIALIZA A SERIAL
+  Serial.begin(9600); // Inicializa a comunicação serial a 9600 bps
 }
 
 void loop(){
-  temp = analogRead(tempPin); //VARIÁVEL RECEBE A LEITURA DO PINO ANALÓGICO A0
-  temp = temp * 0.48828125; //VARIÁVEL RECEBE O VALOR DA TEMPERATURA VEZES A CONSTANTE
-  Serial.print("Temperatura: "); //EXIBE NO MONITOR SERIAL O TEXTO
-  Serial.print(temp); //EXIBE NO MONITOR SERIAL A TEMPERATURA MEDIDA
-  Serial.print("°C"); //EXIBE NO MONITOR SERIAL O TEXTO
-  Serial.println(); //QUEBRA DE LINHA NA SERIAL
-  delay(1000); //INTERVALO DE 1 SEGUNDO
+  temp = analogRead(sensorPin); // Lê o valor do pino analógico A0 e armazena em 'temp'
+  
+  // O valor lido é multiplicado por uma constante para converter a leitura
+  // em temperatura em graus Celsius. Para o LM35, cada unidade lida equivale a 0,48828125 °C.
+  temp = temp * 0.48828125; 
+  
+  // Exibe a temperatura medida no monitor serial
+  Serial.print("Temperatura: "); // Exibe o texto "Temperatura: "
+  Serial.print(temp); // Exibe a temperatura medida
+  Serial.print("°C"); // Exibe o texto "°C"
+  Serial.println(); // Quebra de linha no monitor serial
+  
+  delay(1000); // Aguarda 1 segundo antes de realizar uma nova leitura
 }
