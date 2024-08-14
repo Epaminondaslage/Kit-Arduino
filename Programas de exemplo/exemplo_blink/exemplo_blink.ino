eec0be51da13ce70f19b979de09a53b9b83af49a
Epaminondas Lage <44790000+Epaminondaslage@users.noreply.github.com>
2024-08-14 08:55:26 -0300

    Update exemplo_blink.ino

--- Programas de exemplo/exemplo_blink/exemplo_blink.ino
+++ Programas de exemplo/exemplo_blink/exemplo_blink.ino
@@ -1,11 +1,21 @@
+/**************************************************************************
+ *                 Placa de Desenvolvimento Arduino Uno                   *
+ *                    Exemplo Básico de Piscar um LED                     *
+ * Elaborado/Adptado por [Seu Nome ou Epaminondas Lage]                   *
+ *                                                                        *
+ * Este código pisca o LED embutido na placa Arduino UNO conectado ao      *
+ * pino 13 em intervalos de 1 segundo. É um exemplo simples para iniciar  *
+ * o aprendizado sobre controle de hardware com o Arduino.                *
+ **************************************************************************/
+
 void setup() {
-pinMode(13, OUTPUT); //DECLARA O PINO 13 COMO SENDO SAÍDA
+    pinMode(13, OUTPUT); // Declara o pino 13 como sendo uma saída
 }
+
 void loop() {
+    digitalWrite(13, HIGH); // Energiza o pino 13 e o LED da placa acende
+    delay(1000); // Intervalo de 1 segundo (1000 milissegundos = 1 segundo)
 
-digitalWrite(13, HIGH); //ENERGIZA O PINO 13 E O LED DA PLACA ACENDE
-delay(1000); //INTERVALO DE 1 SEGUNDO (1000 MILISEGUNDOS = 1 SEGUNDO)
-
-digitalWrite(13, LOW); //DESENERGIZA O PINO 13 E O LED DA PLACA APAGA
-delay(1000); //INTERVALO DE 1 SEGUNDO
+    digitalWrite(13, LOW); // Desenergiza o pino 13 e o LED da placa apaga
+    delay(1000); // Intervalo de 1 segundo
 }
